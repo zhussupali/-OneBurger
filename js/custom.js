704d73c19d8b5bda509856013ee6748fbe089182
zhussupali <zhussupali@users.noreply.github.com>
2020-11-08 20:21:50 +0500

    added some js functionality

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,102 @@ document.getElementById("close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
     document.querySelector("body").style.overflow = "scroll";
 })
+
+let products = [
+                {
+    name: "combo1",
+    tag: "combo1",
+    price: 5.99,
+    inCart: 0
+},
+{
+name: "burger1",
+tag: "burger1",
+price: 3.99,
+inCart: 0
+},
+                {
+    name: "pizza1",
+    tag: "pizza1",
+    price: 4.45,
+    inCart: 0
+},
+                {
+    name: "hotdog1",
+    tag: "hotdog1",
+    price: 2.59,
+    inCart: 0
+}
+                ]
+
+let carts = document.querySelectorAll('.add-cart');
+
+for(let i = 0; i < carts.length; i++){
+    carts[i].addEventListener('click',() => {
+        cartNumbers(products[i])
+        totalCost(products[i])
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    if(productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers  ;
+    }
+}
+
+function cartNumbers(product) {
+    
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+    
+    if(productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+    
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    
+    if(cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems =  {
+            [product.tag]: product
+        }
+        
+    }
+    
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    
+    if(cartCost != null ){
+        cartCost = parseFloat(cartCost)
+        localStorage.setItem("totalCost", cartCost + product.price)
+    } else {
+    localStorage.setItem("totalCost", product.price);
+    }
+    
+}
+
+ onLoadCartNumbers()
